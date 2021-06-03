0b5501725ce7212a047f67586e6da9ba32738612
charleslima <jlima.charles@gmail.com>
2021-06-03 10:56:09 -0300

    Setup initial project structure

---
+++ Pokedex/SupportingFiles/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Pokedex
+//
+//  Created by Charles de Jesus Lima on 03/06/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+    
+}
+
